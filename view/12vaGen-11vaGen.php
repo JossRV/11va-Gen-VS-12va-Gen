8f5a55bb9db2c39be97480f002554e56fae093d4
JossRV <jossrv74@gmail.com>
2021-12-03 21:01:59 -0600

    versus procesadores 12va y 11va Gen

--- view/12vaGen-11vaGen.php
+++ view/12vaGen-11vaGen.php
@@ -9,13 +9,13 @@
                     <div class="col-6">
                         <h4 class ="text-center">Procesadores de 12va generación</h4>
                         <p class="lead texto">
-                            <li><h5>Mejor procesamiento gaming</h5></li>
+                            <ul><li><h5>Mejor procesamiento gaming</h5></li></ul>
                             Tiene 16 núcleosy 24 hilos, este procesador nos permite disfrutar de experiencias de los juegos,
                             inclusive el aumento de FPS (cuadros por segundos), 
                             incluso el aumento de fotogramas por segundo para trasmitir y grabar simultáneamente.
                         </p>
                         <p class="lead texto">
-                            <li><h5>Aumento en el rendimiento de la creación de contenido</h5></li>
+                            <ul><li><h5>Aumento en el rendimiento de la creación de contenido</h5></li></ul>
                             Hablamos de múltiples hilos, por lo que ya hay una capacidad de respuesta rápida y la posibilidad de mover
                             datos a altas velocidades con la nueva memoria RAM DDR5, esto nos permite mejorar la experiencia en la
                             creación de contenido. Hasta 36% más de velocidad en la edición de fotos. Hasta 32% más de velocidad en la
@@ -23,24 +23,24 @@
                             de fotogramas múltiples.
                         </p>
                         <p class="lead texto">
-                            <li><h5>Experiencia overclocking</h5></li>
+                            <ul><li><h5>Experiencia overclocking</h5></li></ul>
                             Mejoras de rendimiento en núcleos eficientes y en la memoria DDR5, esto ya existe en las nuevas versiones de
                             Intel Extreme Tuning Utility (XTU) 7.5.
                         </p>
                         <p class="lead texto">
-                            <li><h5>Mejoras de plataforma de escritorio</h5></li>
+                            <ul><li><h5>Mejoras de plataforma de escritorio</h5></li></ul>
                             Esta nueva generación va a mejorar
                             la experiencia de visualización de contenido entre los juegos y videos. Estos cambios son La cache en mejora
                             de la capacidad de memoria con latencia reducida. Conexión inalámbrica de alta velocidad integrada con Intel
                             killer Wi-Fi 6E, esto nos permite disminuir la demora, la latencia y la perdida de paquetes.
                         </p>
                         <p class="lead texto">
-                            <li><h5>Chipset Intel Serie 600</h5></li>
+                            <ul><li><h5>Chipset Intel Serie 600</h5></li></ul>
                             Intel lo realizo con el fin de mejorar la confianza y el rendimiento a un rápido acceso a los dispositivos
                             periféricos y la red.
                         </p>
                         <p class="lead texto">
-                            <li><h5>Arquitectura, Núcleos P y núcleos E</h5></li>
+                            <ul><li><h5>Arquitectura, Núcleos P y núcleos E</h5></li></ul>
                             Son los primeros que se basan en arquitectura de rendimiento hibrido de Intel, que se combina núcleos de
                             rendimiento con los núcleos eficientes para diseñar un rendimiento escalable de cargas de trabajos entre
                             varios hilos. Esto permite que los núcleos trabajen juntos sin problemas, guiando al sistema operativo para
@@ -51,7 +51,7 @@
                             aprovechar al máximo la nueva arquitectura por lo que debe de utilizarse el Windows 11.
                         </p>
                         <p class="lead texto">
-                            <li><h5>Disponibilidad</h5></li>
+                            <ul><li><h5>Disponibilidad</h5></li></ul>
                             Ya se encuentran disponibles en distribuidores autorizados por Intel, el precio de estos procesadores
                             desbloqueados esta entre los 264 Dólares hasta los 589 dólares, por lo que varía en 5,633.76 pesos
                             mexicanos hasta los 12,569.26.
@@ -60,12 +60,12 @@
                     <div class="col-6">
                         <h4 class="text-center">Procesadores de 11va generación</h4>
                         <p class="lead texto">
-                            <li><h5>Procesamiento del procesador</h5></li>
+                            <ul><li><h5>Procesamiento del procesador</h5></li></ul>
                             Esta contienede 6 núcleos/12 hilos hasta 8 núcleos/16 hilos dependiendo el tipo iN de procesador, con una
                             frecuencia de 4.4 GHz hasta 5 GHz, también tienen un chache de 12 MB hasta 24 MB.
                         </p>
                         <p class="lead texto">
-                            <li><h5>Arquitectura</h5></li>
+                            <ul><li><h5>Arquitectura</h5></li></ul>
                             Mejoras de la tecnologíaque permite una capacidad de trabajo con instrucciones vectoriales de 512 bits AVX-512,
                             Soporta el doble de ancho de banda, ósea hablamos de velocidades de hasta 196 MB/s, el incremento de
                             númerosde instrucciones por ciclo (hasta un 19%), Grafica integrada Intel UHD Graphics, con la capacidad de
@@ -73,7 +73,7 @@
                             encima del máximo.
                         </p>
                         <p class="lead texto">
-                            <li><h5>Rendimiento</h5></li>
+                            <ul><li><h5>Rendimiento</h5></li></ul>
                             El rendimiento multiplicado en tareas de inteligencia artificial, conectividad y portabilidad es llamado el
                             Project Athena, también llamado como Intel EVO. Eso se debe a que requiere la mejora de procesos como la
                             capacidad de despertar en segundos desde los estados de suspensión, mejoras de pantallas a full HD, hablando
@@ -83,19 +83,19 @@
                             y sustituir inclusive a AMD y el procesador grafico Nvida.
                         </p>
                         <p class="lead texto">
-                            <li><h5>Wifi 6 y Thunderbolt</h5></li>
+                            <ul><li><h5>Wifi 6 y Thunderbolt</h5></li></ul>
                             Hablando del Project Athena también se realizo con el fin de mejorar la conectividad y la autonomía de la
                             batería, así es como se mejorará la transferencia de 600 Mbps en los streams, el alcance y la cobertura es
                             mejorado aun con espacios saturados.
                         </p>
                         <p class="lead texto">
-                            <li><h5>Trabajos sin limites</h5></li>
+                            <ul><li><h5>Trabajos sin limites</h5></li></ul>
                             Ediciónde videos mas rápido, contenido creado con edición inteligente y el disfrute de juegos que necesiten
                             con la inteligencia artificial (IA), estas trabajan con las aplicaciones que utilizamos a
                             diario,esto con el fin de optimizar la velocidad y el flujo de tareas de manera simplificada.
                         </p>
                         <p class="lead texto">
-                            <li><h5>Desarrollo para empresas</h5></li>
+                            <ul><li><h5>Desarrollo para empresas</h5></li></ul>
                             Ofrecen seguridad en hardware y capacidad de administración remota moderna, junto a las asombrosas mejoras de
                             desempeño en PCs de clase empresarial para empleados, con ayuda de la conectividad, la IA de optimización y
                             entre otras cosas para poder mejorar la capacidad y la fluidez de trabajo.
